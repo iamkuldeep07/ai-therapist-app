ad0e0f1411d5bd9776779d53d078baac083846f3
iamkuldeep07 <kuldeep45.kd@gmail.com>
2025-08-21 17:39:05 +0530

    Update db.ts

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 import { logger } from "./logger";
 
 const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://kuldeep45kd:MiYfTYaSzzFnn70d@ai-therapist-agent.jislgfj.mongodb.net/?retryWrites=true&w=majority&appName=ai-therapist-agent";
+  process.env.MONGODB_URI || "";
 
 export const connectDB = async () => {
     try {
